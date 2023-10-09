badbdb6345d07dff81af924d54f802e15864b2c4
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-10-09 21:25:01 +0100

    hard-coded list

--- src/components/Tracklist.jsx
+++ src/components/Tracklist.jsx
@@ -1,22 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Tracklist = () => {
-  const [track, setTrack] = useState([
-    {
-      name: 'Track name1',
-      artist: 'Artist name1',
-      album: 'Album name1',
-      id: '1',
-    },
-    {
-      name: 'Track name2',
-      artist: 'Artist name2',
-      album: 'Album name2',
-      id: '2',
-    },
-  ])
+  const [tracks, setTracks] = useState([])
 
-  return <div>Tracklist</div>
+  useEffect(() => {
+    setTracks([
+      {
+        name: 'Song 1',
+        artist: 'Artist name1',
+        album: 'Album name1',
+        id: '1',
+      },
+      {
+        name: 'Song 2',
+        artist: 'Artist name2',
+        album: 'Album name2',
+        id: '2',
+      },
+    ])
+  }, [tracks])
+
+  return (
+    <>
+      <h3>Tracklist</h3>
+      {tracks.map(track => (
+        <div key={track.id}>
+          <span>{track.name}</span> - <span>{track.artist}</span>
+        </div>
+      ))}
+    </>
+  )
 }
 
 export default Tracklist
