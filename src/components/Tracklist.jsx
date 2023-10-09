63071407fd9d9b4e5b6b08dbf653b7574b322e4e
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-10-09 21:09:27 +0100

    tracklist

--- src/components/Tracklist.jsx
+++ src/components/Tracklist.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Tracklist = () => {
-  return (
-    <div>Tracklist</div>
-  )
+  const [track, setTrack] = useState([
+    {
+      name: 'Track name1',
+      artist: 'Artist name1',
+      album: 'Album name1',
+      id: '1',
+    },
+    {
+      name: 'Track name2',
+      artist: 'Artist name2',
+      album: 'Album name2',
+      id: '2',
+    },
+  ])
+
+  return <div>Tracklist</div>
 }
 
 export default Tracklist
